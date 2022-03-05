87d45151a00d42c6fd2fa1ae456a59a1f7111201
Antoine <a.grugeon@rubika-edu.com>
2022-03-05 13:13:04 +0100

    Boyds

---
+++ SchoolingFish/Assets/Scripts/SchoolHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchoolHandler : MonoBehaviour
+{
+    public int startNumberOfFish;
+
+    public List<FishBehaviour> school;
+
+    private void Start()
+    {
+        
+    }
+}
